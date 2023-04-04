40d802f1c384cd3039304ef33428bfb19251b39e
mohammadali.m <mamaliocod@gmail.com>
2023-04-04 14:09:01 -0700

    add login

--- account/forms.py
+++ account/forms.py
@@ -1,11 +1,42 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control'}))
-
-    email = forms.EmailField(widget=forms.EmailInput(
-        attrs={'class': 'form-control', 'placeholder': 'your email'}))
-    password = forms.CharField(label="password", widget=forms.PasswordInput(
-        attrs={'class': 'form-control', 'placeholder': 'your pass'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email= forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'your email'}))
+    password1 = forms.CharField(label="password",widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'your pass'}))
+    password2 = forms.CharField(label="confirm password",widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'your pass'}))
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+#  first email is one of the user field and the right one is the var #
+        if user:
+            raise ValidationError('this email is already in use')
+        return email
+        
+    def clean_username(self):
+        username= self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('this username is already in use')
+        return username
+            
+#  Clean def is used when we want to validate tow parameters that are related to each other  #
+            
+    def clean(self):
+        cd= super().clean()
+        p1= cd.get('password1')
+        p2= cd.get('password2')
+        
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('passwords most match')
+        
+        
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'your pass'}))
+    
+            
--- account/views.py
+++ account/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
-class RegisterView(View):
+class UserRegisterView(View):
     form_class = UserRegistrationForm
     template_name = 'account/register.html'
     def get(self, request):
@@ -16,7 +17,32 @@ class RegisterView(View):
         form= self.form_class(request.POST)
         if form.is_valid():
             cd= form.cleaned_data
-            User.objects.create_user(cd['username'],cd['email'], cd['password'])
+            User.objects.create_user(cd['username'],cd['email'], cd['password1'])
             messages.success(request,'you, registration','success')
             return redirect('home:home')
+            # because if our form contains an error , we render the form again with error messages so that the messages comes
         return render(request, self.template_name, {'form': form})
+    
+    
+    
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+    
+    def get(self, request):
+        form= self.form_class
+        return render(request,self.template_name, {'form': form})
+    
+    def post(self, request):
+        # etelaati ke az tariq method post miado be form midim
+        form= self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request,user)
+                messages.success(request,'you logged in successfully', 'success')
+                return redirect('home:home')
+            messages.error(request,'username or password is incorrect','warning')
+            # because if our form contains an error , we render the form again with error messages so that the messages comes
+        return render(request,self.template_name, {'form':form})
